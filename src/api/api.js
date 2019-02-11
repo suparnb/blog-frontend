330970e681ff0709274cfdfd4eb2fb018a99bf6b
Suparn Badwaik <suparnb@gmail.com>
2019-02-10 22:38:03 -0800

    Update app.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,10 @@
 export default {
 	reviews:  {
-		getAll : 'http://34.73.145.65:3001/review/all',
-		getById: 'http://34.73.145.65:3001/review',
-		create: 'http://34.73.145.65:3001/review/createreview',
+		getAll : 'http://localhost:3001/review/all',
+		getById: 'http://localhost:3001/review',
+		create: 'http://localhost:3001/review/createreview',
 	},
 	comments: {
-		create:  'http://34.73.145.65:3001/review/createComment/'
+		create:  'http://localhost:3001/review/createComment/'
 	}
 }
